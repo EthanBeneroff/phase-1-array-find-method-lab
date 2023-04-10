0739a986968999a4d641ff33c39071841111aad2
Ethan Beneroff <ethanbeneroff@gmail.com>
2023-04-10 13:51:47 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+//create a function to check for a win to be passed to find function
+//should return a year if record === W?
+function theyWon(element, index){
+    //console.log(year,result);
+    if (element.result === "W"){
+        return element.year;
+    };
+    }
+
+
+
+    
+function superbowlWin(record){
+    if(record.find(theyWon) !== undefined){
+        return record.find(theyWon).year;
+    }
+    else{
+        return undefined;
+    }
+  
+}
